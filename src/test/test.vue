cd5151260ea1c7d659a47dcc4129ca57759cf92f
liuyaning <55617334+LYaNing-LSG@users.noreply.github.com>
2025-07-07 16:23:38 +0800

    Update test.vue

--- src/test/test.vue
+++ src/test/test.vue
@@ -3,6 +3,15 @@
         测试
     </div>
     <h2>master分支上的东西</h2>
+    <h1>h1字体</h1>
+    <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
   </template>
   
   <script>
