be397f2aa1752d2ce684337879ecaf177dc56cd1
liuyaning <liuyaning@ouryun.com.cn>
2025-07-07 15:45:21 +0800

    fix:header

--- src/test/test.vue
+++ src/test/test.vue
@@ -2,6 +2,18 @@
     <div class="reset">
         重置
     </div>
+    <div class="header">
+        头部
+        <div class="sider">
+            侧边栏
+        </div>
+        <div class="content">
+            内容
+        </div>
+    </div>
+    <div class="footer">
+        底部
+    </div>
   </template>
   
   <script>
@@ -11,5 +23,16 @@
   </script>
   
   <style>
-
+.reset{
+    background-color: red;
+}
+.header{
+    background-color: blue;
+}
+.sider{
+    background-color: yellow;
+}
+    .content{
+        background-color: pink;
+    }
   </style>
