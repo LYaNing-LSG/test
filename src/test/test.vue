6f423b937b004040335f3a9f1dbd9eb6d76665c0
liuyaning <liuyaning@ouryun.com.cn>
2025-07-07 16:21:06 +0800

    feat:布局容器

--- src/test/test.vue
+++ src/test/test.vue
@@ -2,7 +2,17 @@
     <div class="test">
         测试
     </div>
-    <h2>master分支上的东西</h2>
+    <h2>message分支上的东西</h2>
+    <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
   </template>
   
   <script>
