b6f0e9ef39b6b368806981cf3c3e3efa7aded285
liuyaning <liuyaning@ouryun.com.cn>
2025-07-14 10:52:23 +0800

    feat:描述列表添加

--- src/test/test.vue
+++ src/test/test.vue
@@ -5,19 +5,32 @@
     <h2>master分支上的东西</h2>
     <h1>h1字体</h1>
     <div class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-    <h2>message分支上的东西</h2></div>
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+      <h2>message分支上的东西</h2>
+    </div>
     <div class="common-layout">
     <el-container>
       <el-aside width="200px">Aside</el-aside>
       <el-container>
         <el-header>Header</el-header>
-        <el-main>Main</el-main>
+        <el-main>
+           <el-descriptions title="用户信息">
+    <el-descriptions-item label="用户名">娜美</el-descriptions-item>
+    <el-descriptions-item label="电话">18100000000</el-descriptions-item>
+    <el-descriptions-item label="居住地">上海</el-descriptions-item>
+    <el-descriptions-item label="备注">
+      <el-tag size="small">学生</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="详细地址"
+      >上海黄浦区滨江道12号</el-descriptions-item
+    >
+  </el-descriptions>
+</el-main>
         <el-footer>Footer</el-footer>
       </el-container>
     </el-container>
